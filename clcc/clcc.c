1a8c5a36fea6fb101530376b671e859a7ec8935f
David Castells-Rufas <david.castells@uab.es>
2018-01-26 10:22:17 +0100

    create a parseOptions function to detect the different flags new usage function to give some more info about the possible parameters print log if verbose, even when compilation is successful

--- clcc/clcc.c
+++ clcc/clcc.c
@@ -7,6 +7,44 @@
 
 #include "compiler.h"
 
+int doHelp = 0;
+
+void parseOptions(int argc, char* argv[])
+{
+    for (int i=0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--help") == 0)
+            doHelp = 1;
+    }
+    
+    if (argc < 3 || argc > 4)
+        doHelp = 1;
+}
+
+void usage()
+{
+    printf("Usage: clcc [\"compiler-options\"] input.cl output.ptx\n");
+    printf("Where options can be...\n");
+    printf("\n");
+    printf("-cl-nv-verbose\n");
+    printf("\n");
+    printf("-cl-nv-cstd=<version>\n");
+    printf("   version is the OpenCL version supported such as CL1.0 or CL1.1 ");
+    printf("\n");
+    printf("-cl-nv-maxrregcount <N>\n");
+    printf("   Passed on to ptxas as --maxrregcount <N>\n");
+    printf("   N is a positive integer.\n");
+    printf("   Specify the maximum number of registers that GPU functions can use.\n");
+    printf("\n");
+    printf("-cl-nv-opt-level <N>\n");
+    printf("   Passed on to ptxas as --opt-level <N>\n");
+    printf("   N is a positive integer, or 0 (no optimization).\n");
+    printf("\n");
+    printf("-cl-nv-verbose\n");
+    printf("   Passed on to ptxas as --verbose\n");
+    exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char **argv)
 {
 	// Some test arguments:
@@ -30,11 +68,11 @@ int main(int argc, char **argv)
     struct stat sourceStat;
     size_t size;
 
-    if (argc < 3 || argc > 4)
-    {
-        printf("Usage: clcc [\"compiler-options\"] input.cl output.ptx\n");
-        exit(EXIT_SUCCESS);
-    }
+    parseOptions(argc, argv);
+    
+    if (doHelp)
+        usage();
+    
     sourceFilename = argv[argc - 2];
     binaryFilename = argv[argc - 1];
     if (argc > 3)
@@ -79,7 +117,7 @@ int main(int argc, char **argv)
     lengths[0] = strlen(source);
     result = NvCliCompileProgram(strings, count, lengths, options, &log, &binary);
     
-    if (result != 0)
+    //if (result != 0)
     {
         //printf("\n%s", log); // TODO: replace <program source> with better one (like the drivers do)
          
@@ -92,7 +130,9 @@ int main(int argc, char **argv)
     		temp = strtok (NULL, "\n");    	
     	}	
         NvCliCompileLogFree(log);
-        exit(EXIT_FAILURE);
+        
+        if (result != 0)
+            exit(EXIT_FAILURE);
     }
     assert(binary != NULL);
 
